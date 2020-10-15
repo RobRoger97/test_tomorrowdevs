78a7dfd36c3e015bf13e21d738f2ef414d9d5472
RobRoger97 <72944209+RobRoger97@users.noreply.github.com>
2020-10-15 22:27:26 +0200

    Add files via upload

---
+++ Sum_of_the_first_n_positive_integers.py
@@ -0,0 +1,8 @@
+#Chiede il valore di n all'utente
+n = int(input("Inserire un valore intero positivo di n: "))
+
+#Calcola la somma
+somma = n*(n+1)/2
+
+#Stampa risultato
+print("La somma dei prima", n ,"numeri positivi interi è: ", somma)
