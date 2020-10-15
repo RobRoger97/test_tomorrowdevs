6234b63bd6c03408585cf4c55c9bc99790d39f51
RobRoger97 <72944209+RobRoger97@users.noreply.github.com>
2020-10-15 22:33:44 +0200

    Add files via upload

---
+++ Area_of_a_room.py
@@ -0,0 +1,9 @@
+#Chiede all'utente la lunghezza e la larghezza
+lunghezza = float(input("Inserire lalunghezza in metri: "))
+larghezza = float(input("Inserire la larghezza in metri: "))
+
+#Calcolo dell'area 
+area = lunghezza*larghezza
+
+#Stampa il risultato
+print("L'area della stanza è ", area, "m^2")
